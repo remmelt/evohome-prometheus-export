e2831ee4ee3d05834d924e993fb5c46a6160c474
Jonathan Turner <jt@jtnet.co.uk>
2016-10-29 18:13:54 +0100

    added testing to build

--- docker/build/scripts/go-build.sh
+++ docker/build/scripts/go-build.sh
@@ -3,6 +3,11 @@
 export GOPATH=/tmp && \
 cd /tmp && \
 go get -d github.com/jcmturner/evohome-prometheus-export && \
+go test -v src/github.com/jcmturner/evohome-prometheus-export/...
+if [ $? -ne 0 ]; then
+  echo "Golang tests failed"
+  exit 1
+fi
 cd src/github.com/jcmturner/evohome-prometheus-export && \
 go build -tags netgo && \
 mv evohome-prometheus-export /tmp/output
